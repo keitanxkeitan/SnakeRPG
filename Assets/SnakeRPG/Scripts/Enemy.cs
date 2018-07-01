d65a795eb1abdb70076f1002bbb6fb561063f443
keitanxkeitan <keitanxkeitan@gmail.com>
2018-07-01 20:44:31 +0900

    add utility function which sizes game object according to screen resolution

---
+++ Assets/SnakeRPG/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Util.ResizeGameObjectAccordingToResolution(gameObject, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SnakeRPG/Scripts/Util.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static void ResizeGameObjectAccordingToResolution(GameObject gameObject, float widthRate = -1.0f, float heightRate = -1.0f)
+    {
+        Debug.Assert(gameObject.GetComponent<SpriteRenderer>());
+
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        Vector3 localScale = gameObject.transform.localScale;
+
+        if(widthRate > 0.0f)
+        {
+            localScale.x = (Screen.width * widthRate) / (spriteRenderer.bounds.size.x * 100.0f);
+            if(heightRate <= 0.0f)
+            {
+                localScale.y = localScale.x;
+            }
+        }
+
+        if(heightRate > 0.0f)
+        {
+            localScale.y = (Screen.height * heightRate) / spriteRenderer.bounds.size.y;
+            if(widthRate < 0.0f)
+            {
+                localScale.x = localScale.y;
+            }
+        }
+
+        gameObject.transform.localScale = localScale;
+    }
+}
