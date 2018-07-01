f5c5152f5a597178850be736db9d610a2c14ee4b
keitanxkeitan <keitanxkeitan@gmail.com>
2018-07-01 22:20:44 +0900

    hero movement

---
+++ Assets/SnakeRPG/Scripts/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Util.ResizeGameObjectAccordingToResolution(gameObject, 0.04f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var position = transform.position;
+        position.y = -1.0f;
+        if(Input.touchCount > 0)
+        {
+            var touch = Input.touches[0];
+            if(touch.phase == TouchPhase.Moved)
+            {
+                var prevScreenPosition = touch.position - touch.deltaPosition;
+                var currScreenPosition = touch.position;
+                var prevWorldPosition = Camera.main.ScreenToWorldPoint(prevScreenPosition);
+                var currWorldPosition = Camera.main.ScreenToWorldPoint(currScreenPosition);
+                var deltaWorldPosition = currWorldPosition - prevWorldPosition;
+                position.x += deltaWorldPosition.x;
+            }
+        }
+        transform.position = position;
+	}
+}
